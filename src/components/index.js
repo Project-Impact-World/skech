3d7434b190ce4766771770372f641cb38bb8b480
cameron <camrbo@gmail.com>
2021-08-29 19:12:03 -0700

    added current state to github switching to laptop

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ import React from 'react';
 export { default as Navbar } from './navbar/index';
 export { default as List } from './list/ul';
 export { default as NavLink } from './navbar/navlink';
+export { default as Card } from './card/card';
